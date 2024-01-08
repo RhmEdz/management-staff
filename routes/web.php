19a22a2b5273708d14fa46ba896ac604b7d15521
RhmEdz <vrohim1907@gmail.com>
2024-01-08 23:28:44 +0700

    Second Commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+// admin Dashboard
+Route::get('/admin', function() {
+    return view ('Dashboard');
+});
